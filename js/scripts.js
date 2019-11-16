7d28b396dc8c9428394877811cca129c5ac21db6
Youfei Sun <youfei.sun@appnovation.com>
2019-11-16 02:53:07 -0800

    v0.1

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,59 @@
+var state = 0;  // 0 for not started, 1 for running
+
+
 $(document).ready(function(){
-	console.log("print test");
+	$('.initiative-viewport').scrollTop($('.initiative-bar').height());
+
+	new Sortable(initiativeBar, {
+    	animation: 150,
+    	ghostClass: 'grey-shade'
+	});
+
+	sort();
+
+	$('.button.start').click(function(){
+		if(state == 0){
+			state = 1;
+			updateCurrent();	
+		}
+
+	});
+
+	$('.button.next').click(function(){
+		if(state == 1){
+			moveCurrentToTop();
+			removeBottom();
+			updateCurrent();
+		}
+	});
 });
+
+
+function sort(){
+	$(".initiative-bar .entity").sort(asc_sort).appendTo('.initiative-bar');
+}
+
+function asc_sort(a, b){
+	var int_a = parseInt($(a).find('.initiative').text(), 10);
+	var int_b = parseInt($(b).find('.initiative').text(), 10);
+	if (int_b < int_a){
+		return 1;
+	}else if(int_b > int_a){
+		return -1;
+	}else{
+		return 0;
+	}
+	  
+}
+
+function updateCurrent(){
+	$('.initiative-bar .entity:last-child').clone().appendTo('.current-entity');
+}
+
+function moveCurrentToTop(){
+	$('.current-entity .entity').prependTo('.initiative-bar');
+}
+
+function removeBottom(){
+	$('.initiative-bar .entity:last-child').remove();
+}
